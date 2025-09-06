9cef195fff20c55d922ef3a2e654a2618788643c
Aruldass A <aruldass.as@gmail.com>
2025-09-06 21:25:18 +0200

    changes

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -6,5 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent {
-  
+  selectedTab: string = 'openai';
+  profile = {
+    name: 'Aruldass A',
+    photoUrl: 'assets/Aruldass_img.jpeg',
+    designation: 'Full-stack and AI front-end developer',
+    linkedin: 'https://www.linkedin.com/in/aruldass-a-874093a1/',
+    github: 'https://github.com/Aruldass-as',
+    gmail: 'aruldass.as@gmail.com',
+    phone: '+49 15212027492',
+    mail:'admin@codecraft.life'
+  };
+
+  selectTab(tab: string) {
+    this.selectedTab = tab;
+  }
 }
